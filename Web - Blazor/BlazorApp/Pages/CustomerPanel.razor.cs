e6d3822f14e0c499e937ad934b601681510e040e
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2022-02-03 21:54:06 +0200

    Front-end Customer Panel
    - Create razor page for customer panel
    - Create table for customers
    - Fill customer table with data from db

---
+++ Web - Blazor/BlazorApp/Pages/CustomerPanel.razor.cs
@@ -0,0 +1,24 @@
+﻿using BlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp.Data.Services;
+
+namespace BlazorApp.Pages
+{
+    public partial class CustomerPanel
+    {
+        ServiceResponse<List<Customer>> customersResponse = new ServiceResponse<List<Customer>>();
+        List<Customer> customers = new List<Customer>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            customersResponse = await CustomerService.GetAllCustomers(); 
+            if(customersResponse.Success)
+            {
+                customers = customersResponse.Data;
+            }
+        }
+    }
+}
